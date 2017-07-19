61d53fb4b24a7fb7f670eaebc81032835b7d4589
Irvingkk <irvingyyao@gmail.com>
2017-07-19 19:36:23 -0400

    Add files via upload

---
+++ main.cc
@@ -0,0 +1,27 @@
+//
+//  main.cc
+//  item
+//
+//  Created by 姚博伦 on 2017/7/19.
+//  Copyright © 2017年 姚博伦. All rights reserved.
+//
+
+#include <stdio.h>
+#include "gameplay.h"
+#include <iostream>
+int main(){
+    Gameplay newgame = Gameplay();
+    string command;
+    while(cin >> command){
+        if(command == "q"){
+            brea
+        }
+    }
+    
+}
+
+
+
+
+
+
